c0058b7eb3f05f82bb8ca53a7e9a7899f77a9ddb
noob <1833671670@qq.com>
2019-06-22 19:07:36 +0800

    购买跳转链接

--- data/tpl/app/ewei_shopv2/default/mobile/goods/agency.tpl.php
+++ data/tpl/app/ewei_shopv2/default/mobile/goods/agency.tpl.php
@@ -1,26 +1,26 @@
 <?php defined('IN_IA') or exit('Access Denied');?><?php (!empty($this) && $this instanceof WeModuleSite) ? (include $this->template('_header', TEMPLATE_INCLUDEPATH)) : (include template('_header', TEMPLATE_INCLUDEPATH));?>
 <style>
-    .fui-icon-col.external.before:before{
-        content: '';
-        position: absolute;
-        top: 1rem;
-        bottom: 1rem;
-        left: 0;
-        width: 1px;
-        background-color: #ebebeb;
-        display: block;
-        z-index: 15;
-    }
+	.fui-icon-col.external.before:before{
+		content: '';
+		position: absolute;
+		top: 1rem;
+		bottom: 1rem;
+		left: 0;
+		width: 1px;
+		background-color: #ebebeb;
+		display: block;
+		z-index: 15;
+	}
 
 </style>
 <div class='fui-page  fui-page-current'>
-    <div class="fui-header">
-        <div class="fui-header-left">
-            <a class="back" onclick='location.back()'></a>
-        </div>
-        <div class="title">代理</div>
-        <div class="fui-header-right"></div>
-    </div>
+	<div class="fui-header">
+		<div class="fui-header-left">
+			<a class="back" onclick='location.back()'></a>
+		</div>
+		<div class="title">代理</div>
+		<div class="fui-header-right"></div>
+	</div>
 	<a class="fui-goods-item" data-goodsid="14"  data-type="1" data-nocache="true" style="position: relative;margin-top: 2rem;">
 		<div class="detail">
 			<div class="name" style="color: #000000;"><?php  echo htmlspecialchars_decode($info['content'])?></div>
@@ -28,7 +28,7 @@
 	</a>
 
 	<?php  if(is_array($goods)) { foreach($goods as $g) { ?>
-	<a class="fui-goods-item" data-goodsid="14" href="<?php  echo mobileUrl('goods/detail', ['id' => $g['id']])?>" data-type="1" data-nocache="true" style="position: relative;">
+	<a class="fui-goods-item" data-goodsid="14" href="" data-type="1" data-nocache="true" style="position: relative;">
 		<div class="image   triangle" data-text="热销" data-lazyloaded="true" style="background-image: url('<?php  echo tomedia($g['thumb'])?>');">
 		</div>
 		<div class="detail">
@@ -36,17 +36,17 @@
 			<p class="productprice noheight"></p>
 			<div class="price buy">
 				<span class="text" style="color: #ff5555;"><p class="minprice">¥<?php  echo $g['marketprice'];?></p></span>
-				<span class="buy buybtn-1" id="buy-btn" style="border-color: #ff5555;color:  #fff">购买</span>
+				<span class="buy buybtn-1 buy-btn" style="border-color: #ff5555;color:  #fff">购买</span>
 			</div>
 		</div>
 	</a>
 	<?php  } } ?>
-    <?php (!empty($this) && $this instanceof WeModuleSite) ? (include $this->template('_copyright', TEMPLATE_INCLUDEPATH)) : (include template('_copyright', TEMPLATE_INCLUDEPATH));?>
+	<?php (!empty($this) && $this instanceof WeModuleSite) ? (include $this->template('_copyright', TEMPLATE_INCLUDEPATH)) : (include template('_copyright', TEMPLATE_INCLUDEPATH));?>
 </div>
 <script>
 	$(function () {
-		$('#buy-btn').click(function () {
-			window.location = "<?php  echo mobileUrl('goods/detail', ['id' => $goods['id']])?>";
+		$('.buy-btn').click(function () {
+			window.location = "<?php  echo mobileUrl('order/create', ['id' => $g['id']])?>";
 		})
 	})
 </script>
